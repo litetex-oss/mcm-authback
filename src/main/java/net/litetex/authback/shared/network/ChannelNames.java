46280810c0d1a3307e48ba70f255787da4138112
litetex <40789489+litetex@users.noreply.github.com>
2025-12-07 21:38:51 +0100

    [1.21.11] Fix compilation

--- src/main/java/net/litetex/authback/shared/network/ChannelNames.java
+++ src/main/java/net/litetex/authback/shared/network/ChannelNames.java
@@ -1,18 +1,18 @@
 package net.litetex.authback.shared.network;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 
 public final class ChannelNames
 {
-	public static final ResourceLocation FALLBACK_AUTH = create("fallback_auth_v1");
+	public static final Identifier FALLBACK_AUTH = create("fallback_auth_v1");
 	
-	public static final ResourceLocation SYNC_S2C = create("sync_s2c_v1");
-	public static final ResourceLocation SYNC_C2S = create("sync_c2s_v1");
+	public static final Identifier SYNC_S2C = create("sync_s2c_v1");
+	public static final Identifier SYNC_C2S = create("sync_c2s_v1");
 	
-	private static ResourceLocation create(final String name)
+	private static Identifier create(final String name)
 	{
-		return ResourceLocation.fromNamespaceAndPath("authback", name);
+		return Identifier.fromNamespaceAndPath("authback", name);
 	}
 	
 	private ChannelNames()
