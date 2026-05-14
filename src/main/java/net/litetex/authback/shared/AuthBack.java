d14bf01b4a075977345d6f4583080955eaff72fb
litetex <40789489+litetex@users.noreply.github.com>
2026-05-15 01:36:11 +0200

    Improve performance

--- src/main/java/net/litetex/authback/shared/AuthBack.java
+++ src/main/java/net/litetex/authback/shared/AuthBack.java
@@ -1,9 +1,8 @@
 package net.litetex.authback.shared;
 
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import net.litetex.authback.shared.config.Configuration;
 import net.litetex.authback.shared.io.AuthBackDirEnsurer;
@@ -11,8 +10,7 @@ import net.litetex.authback.shared.io.AuthBackDirEnsurer;
 
 public abstract class AuthBack
 {
-	private static final Map<String, Configuration> CONFIG_DEDUPLICATION =
-		Collections.synchronizedMap(new HashMap<>());
+	private static final Map<String, Configuration> CONFIG_DEDUPLICATION = new ConcurrentHashMap<>(1);
 	
 	protected final Configuration lowLevelConfig;
 	protected final Path authbackDir;
